ab5829078af44f6349e3021f3b4aa2c05e5900e4
James Summergreene <james.summergreene@gmail.com>
2022-07-26 12:38:06 +1000

    index.js settup, api call added and post route. scss global variables added

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,11 @@ import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
 
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
 alert("I EXIST")
 console.log("CHANGE!!");
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,65 @@
+// Require
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+// .env
+const dotenv = require('dotenv')
+dotenv.config()
+
+// API Key
+const textapi = process.env.MC_API_KEY
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Cors
+const cors = require('cors')
+app.use(cors());
+
+// Express
 const app = express()
-
 app.use(express.static('dist'))
+// console.log(__dirname)
 
-console.log(__dirname)
-
+// Routes
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+ // Post route for user input
+ app.post("/post", async (req, res)=>{
+    try {
+        const userInput = await req.body.text
+        const data = await getMeaningCloudData(userInput) // Send user input data to api call function
+        res.send(data)
+    }catch(error){
+        console.log("error", error)
+    }
+ })
+
+  // Meaning Cloud API call
+  const getMeaningCloudData = async (text)=>{
+    const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key="
+    const apiKey = process.env.API_KEY
+    const result = await fetch(`${baseUrl}${apiKey}&txt=${text}&lang=en`)
+    try {
+        const response = await result.json()
+        //Return the analysed result from meaningcloud service
+        console.log(response)
+        return response
+    } catch(error) {
+        console.log("error", error)
+    }
+} 
